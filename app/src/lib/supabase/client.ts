50beda975bc9fad69908434f59cd937c2ccbf9b0
Nishant More <nishantrmore9577@gmail.com>
2026-08-13 20:27:37 +0530

    feat(supabase): establish client foundation

---
+++ app/src/lib/supabase/client.ts
@@ -0,0 +1,9 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+export function createClient() {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabasePublishableKey =
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+
+  return createBrowserClient(supabaseUrl, supabasePublishableKey);
+}
